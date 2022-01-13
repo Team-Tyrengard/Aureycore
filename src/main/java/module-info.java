6f8f8d32e00a54c0c9fe07549a8d27cae6054ea1
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 18:29:32 +0800

    moved foundation package to separate project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,15 @@
 module com.tyrengard.aureycore {
     requires org.bukkit;
     requires java.logging;
-    requires morphia.core;
-    requires org.mongodb.driver.sync.client;
-    requires org.mongodb.driver.core;
-    requires org.mongodb.bson;
     requires org.jetbrains.annotations;
     exports com.tyrengard.aureycore.common.anvilevents;
     exports com.tyrengard.aureycore.common.stringformat;
     exports com.tyrengard.aureycore.common.interfaces;
     exports com.tyrengard.aureycore.common.struct;
     exports com.tyrengard.aureycore.common.utils;
-    exports com.tyrengard.aureycore.foundation;
     exports com.tyrengard.aureycore.playerstate;
     exports com.tyrengard.aureycore.damageindicator;
     exports com.tyrengard.aureycore.customguis;
     exports com.tyrengard.aureycore;
     exports com.tyrengard.aureycore.common.armorevents;
-    exports com.tyrengard.aureycore.foundation.db;
 }
